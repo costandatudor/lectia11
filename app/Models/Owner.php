72f0a99d4c3161e18bf92e6de5f1cfe8b746709c
tudorG4 <costadna.tudor@elevcihcahul.md>
2025-12-04 08:18:39 +0200

    lectia11

---
+++ app/Models/Owner.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Owner extends Model
+{
+    protected $table = 'owners';
+    protected $fillable = ['id', 'name', 'car_id'];
+
+    public function car()
+    {
+        return $this->belongsTo(Car::class);
+    }
+}
